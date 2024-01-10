1fa9713f2293b54e9bcd10654bedff849caa1bb4
Mart Rietdijk <JM.rietdijk@student.han.nl>
2024-01-10 11:02:59 +0100

    added new diagrams

---
+++ README.md
@@ -0,0 +1,3 @@
+# Technisch leerdoel
+
+In deze repo is de broncode + resultaten te vinden van mijn technisch leerdoel voor het WoR-project. Deze readme geeft uitleg over 
---
+++ velodyne_drivers_on/main.r
@@ -0,0 +1,28 @@
+library(ggplot2)
+
+data <- read.csv("lidar_data2.csv")
+
+lenghts <- c()
+times <- c()
+
+curr_time <- 0
+curr_length <- 0
+
+
+for (i in 1:nrow(data)) {
+  if (floor(data$Time[i]) == curr_time) {
+    curr_length <- curr_length + data$Length[i]
+  } else {
+    lenghts <- c(lenghts, curr_length)
+    times <- c(times, curr_time)
+    curr_time <- floor(data$Time[i])
+    curr_length <- data$Length[i]
+  }
+}
+data <- data.frame(times, lenghts)
+
+ggplot() +
+  geom_line(aes(x = times, y = lenghts), color = "blue") +
+  xlab("Time in seconds") +
+  ylab("Length of messages in bytes") +
+  ggtitle("Length of messages over time")
